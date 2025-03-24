dd842a069dc665e79983a08c7be9bb8523537bd2
WarnerU22 <Warnerussery22@gmail.com>
2025-03-24 12:21:17 -0500

    add hi and bye

--- pythonfile2.py
+++ pythonfile2.py
@@ -1,1 +1,9 @@
-print("New Python File!!!")
+print("Welcome to the GitHub Repo")
+
+userchoice = input("What Would You Like To Do \n (1):Say Hi \n (2):Say Bye \n")
+if userchoice == '1':
+     print("HEY")
+if userchoice == '2':
+    print("BYE")
+
+        
